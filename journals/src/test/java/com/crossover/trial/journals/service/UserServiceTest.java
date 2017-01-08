753cdd95281cb630ffc72ec71177e8bf4b45cb07
AnghelLeonard <leoprivacy@yahoo.com>
2017-01-08 09:13:02 +0200

    Added tests

--- journals/src/test/java/com/crossover/trial/journals/service/UserServiceTest.java
+++ journals/src/test/java/com/crossover/trial/journals/service/UserServiceTest.java
@@ -1,16 +1,11 @@
 package com.crossover.trial.journals.service;
 
+import com.crossover.trial.journals.model.Subscription;
+import com.crossover.trial.journals.model.User;
+import static com.crossover.trial.journals.utils.Utils.getUser;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
-import java.util.List;
-import java.util.Optional;
-
-import com.crossover.trial.journals.model.Journal;
-import com.crossover.trial.journals.model.Publisher;
-import com.crossover.trial.journals.model.User;
-import com.crossover.trial.journals.repository.PublisherRepository;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,27 +20,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserServiceTest {
-   
-    @Autowired
-    private JournalService journalService;
 
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private PublisherRepository publisherRepository;
+    @Test
+    public void findByIdTest() {
+        User user = userService.findById(1L);
+        assertNotNull(user);
+    }
 
     @Test
     public void subscribeUserToCategory() {
-        /*
-        List<Journal> journals = journalService.listAll(getUser("user1"));
-        assertNotNull(journals);
-        assertEquals(1, journals.size());
 
-        assertEquals(new Long(1), journals.get(0).getId());
-        assertEquals("Medicine", journals.get(0).getName());
-        assertEquals(new Long(1), journals.get(0).getPublisher().getId());
-        assertNotNull(journals.get(0).getPublishDate());
-*/
-    }             
+        User user = getUser("user1");
+        userService.subscribe(user, 1L);
+
+        List<Subscription> subscribtions = user.getSubscriptions();
+        assertEquals(2, subscribtions.size());
+    }
 }
